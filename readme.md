f3b092c041c961e8f45dc591952d6357635a3517
Ernesto Mancebo <ernesmancebo@gmail.com>
2014-12-24 09:53:25 -0400

    My job with mustache is done

---
+++ readme.md
@@ -0,0 +1,2 @@
+This was just for test what could I do with mustache. As I can see, it's just for display information, not such as a MVVM framework.
+So, IMHO, mustache is uneficient.
--- venues.js
+++ venues.js
@@ -1,13 +1,19 @@
 var PRICE_RANGE = {
-    CHEAP: "Cheap",
-    MODERATE: "Moderate",
-    EXPENSIVE: "Expensive",
-    LUXURY: "Luxury"
+	CHEAP: "Cheap",
+	MODERATE: "Moderate",
+	EXPENSIVE: "Expensive",
+	LUXURY: "Luxury"
 };
 
 var venue = {
-    "name": "Fatty McFat Pizza",
-    "location": [50.0, 45.0],
-    "tags": ["pizza", "fast food"],
-    "price_range": PRICE_RANGE.CHEAP
+	"name": "Fatty McFat Pizza",
+	"location": [50.0, 45.0],
+	"tags": ["pizza", "fast food"],
+	"price_range": PRICE_RANGE.CHEAP,
+	"locationParser": function() {
+		if (this.location.length == 2)
+			return "[ " + this.location[0] + ", " + this.location[1] + " ]";
+		else
+			return "invalid location array!!";
+	}
 };
