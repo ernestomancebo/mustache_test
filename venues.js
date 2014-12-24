b39317473458a6f69c7efc23ce17934806d1134d
Ernesto Mancebo <ernesmancebo@gmail.com>
2014-12-23 21:22:05 -0400

    Testing modular mustache library

---
+++ venues.js
@@ -0,0 +1,13 @@
+var PRICE_RANGE = {
+    CHEAP: "Cheap",
+    MODERATE: "Moderate",
+    EXPENSIVE: "Expensive",
+    LUXURY: "Luxury"
+};
+
+var venue = {
+    "name": "Fatty McFat Pizza",
+    "location": [50.0, 45.0],
+    "tags": ["pizza", "fast food"],
+    "price_range": PRICE_RANGE.CHEAP
+};
